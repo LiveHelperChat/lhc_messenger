04823e43c96d04b98057488ca3fbe3f519147fd8
samapraku <samuel-adu-ansere.apraku@s2017.tu-chemnitz.de>
2020-02-20 17:51:30 +0100

    Fix tab number indicator

--- lib/widget/chat_number_indicator.dart
+++ lib/widget/chat_number_indicator.dart
@@ -11,16 +11,21 @@ class ChatNumberIndcator extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return
-       Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+       Stack(
         children: <Widget>[
-          Text(title,textAlign: TextAlign.center,style: new TextStyle(fontSize: 12.0),),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child:Text(title,textAlign: TextAlign.center,
+            style:  TextStyle(
+              fontSize: 12.0),), 
+          ),          
            Offstage(
             offstage: offstage,
-            child:  Container(
-              padding: const EdgeInsets.symmetric(vertical: 5.0,horizontal: 5.0),
-              margin: const EdgeInsets.all( 2.0),
+            child: Align(
+            alignment: Alignment.center,
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 4.0,horizontal: 6.0),
+              margin: const EdgeInsets.only(bottom: 8.0),
               decoration: new BoxDecoration(
                 shape: BoxShape.circle,
                 color: Colors.white,
@@ -28,11 +33,12 @@ class ChatNumberIndcator extends StatelessWidget{
               child: new Text(
                 number,
                 style: new TextStyle(
-                  fontSize: 8.0,
+                  fontSize: 9.0,
                   color: Theme.of(context).primaryColorDark,
                 ),
                 textAlign: TextAlign.center,
               ),
+            ),
             ),
           ),
         ],
