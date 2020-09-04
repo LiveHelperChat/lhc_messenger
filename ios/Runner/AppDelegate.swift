1b28ba5742f9fe9b9e2dfb5644819339cd152cbe
Remigijus Kiminas <remdex@gmail.com>
2020-09-04 01:20:11 -0400

    IOS Testing

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,19 @@
 import UIKit
 import Flutter
-
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  _ application: UIApplication,
+  didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  GeneratedPluginRegistrant.register(with: self)
+  if #available(iOS 10.0, *) {
+    UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
   }
+  return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+    @available(iOS 10.0, *)
+    override func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert,.sound,.badge])
+    }
 }
