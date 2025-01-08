9064fffea8b1043bab6bcd4f30430e0098418682
Remigijus Kiminas <remdex@gmail.com>
2025-01-08 15:20:16 -0500

    Build test

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,25 +1,19 @@
 import UIKit
 import Flutter
-import FirebaseCore
-import FirebaseMessaging
-
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-    override func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        FirebaseApp.configure()
-        UNUserNotificationCenter.current().delegate = self
-        application.registerForRemoteNotifications()
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
-
-    override func application(
-        _ application: UIApplication,
-        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-    ) {
-        Messaging.messaging().apnsToken = deviceToken
-        super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+  override func application(
+  _ application: UIApplication,
+  didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+  GeneratedPluginRegistrant.register(with: self)
+  if #available(iOS 10.0, *) {
+    UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+  }
+  return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+    @available(iOS 10.0, *)
+    override func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert,.sound,.badge])
     }
 }
