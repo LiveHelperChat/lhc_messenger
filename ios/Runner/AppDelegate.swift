752c65fa870515331a3e7952bdd8c9310f78f8db
Remigijus Kiminas <remdex@gmail.com>
2025-01-08 23:20:24 -0500

    Attempt ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,19 +1,21 @@
 import UIKit
 import Flutter
+import FirebaseCore
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
-  _ application: UIApplication,
-  didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-  GeneratedPluginRegistrant.register(with: self)
-  if #available(iOS 10.0, *) {
-    UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-  }
-  return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-    @available(iOS 10.0, *)
-    override func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.alert,.sound,.badge])
+
+    // Check if Firebase has already been configured. This is crucial to prevent
+    // multiple FirebaseApp instances, which can lead to crashes or unexpected behavior.
+    if FirebaseApp.app() == nil {
+        FirebaseApp.configure()
     }
+
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 }
