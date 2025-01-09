beb838f332f026a5c4c22fd5f4021538b2655c81
Remigijus Kiminas <remdex@gmail.com>
2025-01-09 01:34:59 -0500

    Try upgrade new app

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,19 +1,29 @@
 import UIKit
 import Flutter
+import FirebaseCore
+import flutter_downloader
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
-  _ application: UIApplication,
-  didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-  GeneratedPluginRegistrant.register(with: self)
-  if #available(iOS 10.0, *) {
-    UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+
+    // Check if Firebase has already been configured. This is crucial to prevent
+    // multiple FirebaseApp instances, which can lead to crashes or unexpected behavior.
+    if FirebaseApp.app() == nil {
+        FirebaseApp.configure()
+    }
+
+    GeneratedPluginRegistrant.register(with: self)
+    FlutterDownloaderPlugin.setPluginRegistrantCallback(registerPlugins)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-  return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-    @available(iOS 10.0, *)
-    override func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.alert,.sound,.badge])
+}
+
+private func registerPlugins(registry: FlutterPluginRegistry) {
+    if (!registry.hasPlugin("FlutterDownloaderPlugin")) {
+       FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin")!)
     }
 }
