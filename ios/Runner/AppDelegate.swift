46c4bc2ce2ec0a79668664f9063c6c9ae0ff07dc
Remigijus Kiminas <remdex@gmail.com>
2025-01-09 02:21:03 -0500

    Merge into one

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,8 @@
 import UIKit
 import Flutter
+import FirebaseCore
+import flutter_downloader
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -7,6 +10,7 @@ import Flutter
   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
   GeneratedPluginRegistrant.register(with: self)
+  FlutterDownloaderPlugin.setPluginRegistrantCallback(registerPlugins)
   if #available(iOS 10.0, *) {
     UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
   }
@@ -17,3 +21,39 @@ import Flutter
         completionHandler([.alert,.sound,.badge])
     }
 }
+
+private func registerPlugins(registry: FlutterPluginRegistry) {
+    if (!registry.hasPlugin("FlutterDownloaderPlugin")) {
+       FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin")!)
+    }
+}
+
+/*import UIKit
+import Flutter
+import FirebaseCore
+import flutter_downloader
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+
+    // Check if Firebase has already been configured. This is crucial to prevent
+    // multiple FirebaseApp instances, which can lead to crashes or unexpected behavior.
+    if FirebaseApp.app() == nil {
+        FirebaseApp.configure()
+    }
+
+    GeneratedPluginRegistrant.register(with: self)
+    FlutterDownloaderPlugin.setPluginRegistrantCallback(registerPlugins)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
+
+private func registerPlugins(registry: FlutterPluginRegistry) {
+    if (!registry.hasPlugin("FlutterDownloaderPlugin")) {
+       FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin")!)
+    }
+}*/
