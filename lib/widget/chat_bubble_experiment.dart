7bc0ab79118be8182aa8eac477835278405185f1
Remigijus Kiminas <remdex@gmail.com>
2025-02-28 06:49:14 -0500

    Copy widget

--- lib/widget/chat_bubble_experiment.dart
+++ lib/widget/chat_bubble_experiment.dart
@@ -2,6 +2,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:intl/intl.dart';
 import 'package:livehelp/model/model.dart';
@@ -68,75 +69,100 @@ class ChatBubbleExperiment extends StatelessWidget {
           padding: const EdgeInsets.only(right: 8.0),
           child: Text(message.name_support!),
         ),
-        Container(
-          constraints: const BoxConstraints(minWidth: 50.0),
-          margin: margin,
-          padding: const EdgeInsets.all(4.0),
-          decoration: BoxDecoration(
-            color: bg,
-            borderRadius: radius,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(right: 4.0, bottom: 4.0),
-
-                child: messageMediaType == MessageMediaType.Text
-                    ? Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Html(
-                      data: message.msg,
-                    ),
-                    SelectableText(
-                      // Strip HTML tags for selectable plain text
-                      message.msg?.replaceAll(RegExp(r'<[^>]*>'), '') ?? '',
-                      style: TextStyle(height: 0, fontSize: 0, color: Colors.transparent),
-                    ),
-                  ],
-                )
-                    : messageMediaType == MessageMediaType.Audio
-                    ? MyAudioMessageWidget(
-                  message: message, link: link,
-                )
-                    : messageMediaType == MessageMediaType.Image
-                    ? ImageMessageWidget(
-                  message: message, link: link,
-                )
-                    : messageMediaType == MessageMediaType.File
-                    ? FileMessageWidget(
-                  key: ValueKey(message.id.toString()),
-                  message: message,
-                )
-                    : Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Html(data: message.msg),
-                    SelectableText(
-                      // Strip HTML tags for selectable plain text
-                      message.msg?.replaceAll(RegExp(r'<[^>]*>'), '') ?? '',
-                      style: TextStyle(height: 0, fontSize: 0, color: Colors.transparent),
-                    ),
-                  ],
+        GestureDetector(
+          onLongPress: () {
+            if (messageMediaType == MessageMediaType.Text || message.msg != null) {
+              _showCopyDialog(context, message.msg ?? "");
+            }
+          },
+          child: Container(
+            constraints: const BoxConstraints(minWidth: 50.0),
+            margin: margin,
+            padding: const EdgeInsets.all(4.0),
+            decoration: BoxDecoration(
+              color: bg,
+              borderRadius: radius,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(right: 4.0, bottom: 4.0),
+                  child: messageMediaType == MessageMediaType.Text
+                      ? Html(data: message.msg,)
+                      : messageMediaType == MessageMediaType.Audio
+                      ? MyAudioMessageWidget(
+                    message: message, link: link,
+                  )
+                      : messageMediaType == MessageMediaType.Image
+                      ? ImageMessageWidget(
+                    message: message, link: link,
+                  )
+                      : messageMediaType == MessageMediaType.File
+                      ? FileMessageWidget(
+                    key: ValueKey(message.id.toString()),
+                    message: message,
+                  )
+                      : Html(data: message.msg),
                 ),
-
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0),
-                child: Text(
-                  dateFormatter.format(DateTime.fromMillisecondsSinceEpoch(
-                      message.time! * 1000)),
-                  style: const TextStyle(
-                    color: Colors.black38,
-                    fontSize: 10.0,
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: Text(
+                    dateFormatter.format(DateTime.fromMillisecondsSinceEpoch(
+                        message.time! * 1000)),
+                    style: const TextStyle(
+                      color: Colors.black38,
+                      fontSize: 10.0,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         )
       ],
+    );
+  }
+
+  void _showCopyDialog(BuildContext context, String text) {
+    // Strip HTML tags for clean copying
+    String cleanText = text.replaceAll(RegExp(r'<[^>]*>'), '');
+
+    showDialog(
+      context: context,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: Text("Message Options"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ListTile(
+                leading: Icon(Icons.copy),
+                title: Text("Copy Text"),
+                onTap: () {
+                  Clipboard.setData(ClipboardData(text: cleanText));
+                  Navigator.of(dialogContext).pop();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text("Message copied to clipboard"),
+                      duration: Duration(seconds: 2),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(dialogContext).pop();
+              },
+              child: Text("Cancel"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
